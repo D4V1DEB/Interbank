bc539f5d673ccfec4206d5938fd2b2144bc3ae7d
David Espinoza <158826167+D4V1DEB@users.noreply.github.com>
2024-07-26 10:51:28 -0500

    Delete Interbank directory

--- Interbank/web/Carga/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById("introVideo");
-    video.play();
-    video.addEventListener("ended", function() {
-    window.location.href = "../CrearCuenta/formulario.jsp";
-    });
-});
